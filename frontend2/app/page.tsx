740f0da019b97b7f0cdd59c3d8f35a09f5562167
maksimkulis <maksimkulis@yandex.ru>
2023-06-24 18:28:42 +0300

    fix

---
+++ frontend2/app/page.tsx
@@ -0,0 +1,10 @@
+import FileUpload from "@/components/FileUpload";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-10">
+      <FileUpload />
+    </div>
+  );
+}
---
+++ frontend2/components/FileUpload.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useRef, useState } from "react";
+import { CloudArrowUpIcon } from "@heroicons/react/24/outline";
+import classNames from "classnames";
+import axios from "axios";
+const FileUpload = () => {
+  const [fileList, setFileList] = useState<File[] | null>(null);
+  const [shouldHighlight, setShouldHighlight] = useState(false);
+
+  const preventDefaultHandler = (e: React.DragEvent<HTMLElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleUpload = async () => {
+    // const UPLOAD_URL = "YOUR URL HERE";
+    console.log("opa")
+    
+    const UPLOAD_URL = "http://localhost:8001/upload";
+    const data = new FormData();
+    for (let file of fileList!) {
+      data.append(file.name, file);
+    }
+    await axios.post(UPLOAD_URL, data);
+  };
+  return (
+    <div
+      className={classNames({
+        "w-full h-96": true,
+        "p-4 grid place-content-center cursor-pointer": true,
+        "text-violet-500 rounded-lg": true,
+        "border-4 border-dashed ": true,
+        "transition-colors": true,
+        "border-violet-500 bg-violet-100": shouldHighlight,
+        "border-violet-100 bg-violet-50": !shouldHighlight,
+      })}
+      onDragOver={(e) => {
+        preventDefaultHandler(e);
+        setShouldHighlight(true);
+      }}
+      onDragEnter={(e) => {
+        preventDefaultHandler(e);
+        setShouldHighlight(true);
+      }}
+      onDragLeave={(e) => {
+        preventDefaultHandler(e);
+        setShouldHighlight(false);
+      }}
+      onDrop={(e) => {
+        preventDefaultHandler(e);
+        const files = Array.from(e.dataTransfer.files);
+        setFileList(files);
+        setShouldHighlight(false);
+      }}
+    >
+      <div className="flex flex-col items-center">
+        {!fileList ? (
+          <>
+            <CloudArrowUpIcon className="w-10 h-10" />
+            <span>
+              <span>Choose a File</span> or drag it here
+            </span>
+          </>
+        ) : (
+          <>
+            <p>Files to Upload</p>
+            {fileList.map((file, i) => {
+              return <span key={i}>{file.name}</span>;
+            })}
+            <div className="flex gap-2 mt-2">
+              <button
+                className="bg-violet-500 text-violet-50 px-2 py-1 rounded-md"
+                onClick={() => {
+                  handleUpload();
+                }}
+              >
+                Upload
+              </button>
+              <button
+                className="border border-violet-500 px-2 py-1 rounded-md"
+                onClick={() => {
+                  setFileList(null);
+                }}
+              >
+                Clear
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FileUpload;
